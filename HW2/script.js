c3023b2287d825219f9344874759e6a168981c7e
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-01-31 22:26:17 -0700

    update index, js, and readme + add to site

---
+++ HW2/script.js
@@ -0,0 +1,17 @@
+console.log("Favorite sites:");
+    console.log("www.youtube.com");
+    console.log("www.reddit.com");
+    console.log("www.artstation.com");
+
+console.log("Favorite Games:");
+    console.log("Destiny 2");
+    console.log("God of War");
+    console.log("Ghost of Tsushima");
+
+console.log("Favorite Artists:");
+    console.log("Vincent Van Gogh");
+        console.log("Van Gogh's artwork is so colorful and bold. The brushstrokes in each piece have so much energy in them. His ability to create such beautiful artwork even in the dark times of his life is inspiring.");
+    console.log("Salvador Dali");
+        console.log("Dali was able to put mystical and magical imagery into his artwork. He was able to bring the viewer into the world of his imagination where anything is possible.");
+    console.log("Alex Ross");
+        console.log("Instead of the standard pen & ink or digital coloring, Alex Ross creates comic book art using his paintbrush. His works such as Kingdom Come, Justice, and Marvels all show off his amazing and realistic artwork of heroes that are often presented in a more cartoon-ish fashion.");
