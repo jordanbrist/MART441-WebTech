068fc25d60b3117e203a8fed422861e89a419936
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-02-22 22:05:08 -0700

    fixed naming and image placement

--- HW5/readme.md
+++ HW5/readme.md
@@ -1,2 +1,4 @@
 # Homework 5
 ## Jordan Brist
+
+This week wasn't too bad. I remember using arrays a lot towards the end of MART 120, so this was a nice refresher. I'm a bit anxious but also excited to build onto it and use the new things that we went over in this week's lesson. The only problem I had with this week's assignment was an error in my naming that caused half of the images to not show up, but it was a simple fix.
--- HW5/script.js
+++ HW5/script.js
@@ -1,14 +1,15 @@
-var imageTag = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10",];
-var imageBack = "images/back.jpg";
+var imageTag = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12", "image13", "image14", "image15", "image16", "image17", "image18", "image19", "image20",];
+var imagePath = ["images/image1.png", "images/image2.png", "images/image3.png", "images/image4.png", "images/image5.png", "images/image6.png", "images/image7.png", "images/image8.png", "images/image9.png", "images/image10.png"];
+var back = "images/back.jpg";
 var images = new Array();
 
-function printImageBack()
+function printBack()
 {
     createRandomImageArray();
 
     for(var i = 0; i < imageTag.length; i++)
     {
-        document.getElementById(imageTag[i]).src= imageBack;
+        document.getElementById(imageTag[i]).src= back;
     }
 }
 
@@ -16,10 +17,9 @@ function printImageBack()
 function createRandomImageArray()
 
 {
-    var imagePath = ["images/image1.png", "images/image2.png", "images/image3.png", "images/image4.png", "images/image5.png", "images/image6.png", "images/image7.png", "images/image8.png", "images/image9.png", "images/image10.png"];
     var count = [0,0,0,0,0,0,0,0,0,0];
 
-    while(images.length < 10)
+    while(images.length < 20)
     {
         var randomNumber = Math.floor(Math.random() * imagePath.length)
         if(count[randomNumber] < 2)
