ab54e3b7763d374d3184d36402877ae0e3dc1c34
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-28 22:12:41 -0600

    trying something

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-            $("button").click(function () {
-                $("#pokeInfo").load("data/pokedex.json", function(responseText){
-                    var pokemon = JSON.parse(responseText);
-                    $("#pokeInfo").html("Number:" + pokemon.num + "Name:" + pokemon.name + "Type:" + pokemon.type[0] + pokemon.type[1] + "Height:" + pokemon.height + "Weight:" + pokemon.weight);
-                });
+    $("button").click(function () {
+        $.getJSON("data/pokedex.json", function (data) {
+            var row;
+            $.each(data, function (i, index) {
+                row = $('<tr/>');
+                row.append("<td>" + index.name + "</td>");
+                row.append("<td>" + index.id + "</td>");
+                row.append("<td>" + index.recclass + "</td>");
+                row.append("<td>" + index.mass + "</td>");
+                $('table').append(row);
             });
         });
+    });
+});
