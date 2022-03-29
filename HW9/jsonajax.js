05e7f290897c41368b38065e89955184bf25a5aa
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-29 00:06:48 -0600

    fixed?

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
     $("button").click(function () {
         $.getJSON("pokedex.json", function (data) {
-            var pokedex;
             $.each(data, function (i, pokemon) {
-                pokedex = $('<tr/>');
-                pokedex.append("<td>" + pokemon.name + "</td>");
-                pokedex.append("<td>" + pokemon.num + "</td>");
-                pokedex.append("<td>" + pokemon.height + "</td>");
-                pokedex.append("<td>" + pokemon.weight + "</td>");
-                $('table').append(pokedex);
+                $('<tr/>').append("<td>" + pokemon.name + "</td>");
+                $('<tr/>').append("<td>" + pokemon.num + "</td>");
+                $('<tr/>').append("<td>" + pokemon.height + "</td>");
+                $('<tr/>').append("<td>" + pokemon.weight + "</td>");
             });
         });
     });
