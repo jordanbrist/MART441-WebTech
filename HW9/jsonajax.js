0257bc05255f4df23b9ad7da0c1e34c224873450
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-28 22:42:10 -0600

    Update jsonajax.js

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -2,14 +2,25 @@ $(document).ready(function () {
     $("button").click(function () {
         $.getJSON("./pokedex.json", function (data) {
             var row;
+            var pokemon;
             $.each(data, function (i, index) {
                 row = $('<tr/>');
-                row.append("<td>" + index.name + "</td>");
-                row.append("<td>" + index.num + "</td>");
-                row.append("<td>" + index.height + "</td>");
-                row.append("<td>" + index.weight + "</td>");
+                row.append("<td>" + pokemon.name + "</td>");
+                row.append("<td>" + pokemon.num + "</td>");
+                row.append("<td>" + pokemon.height + "</td>");
+                row.append("<td>" + pokemon.weight + "</td>");
                 $('table').append(row);
             });
         });
     });
 });
+
+
+    (function($){
+        $.fn.bluey = function () {
+            this.css("background-color", "blue");
+            this.css("color", "white");
+            this.css("font-size", 24);
+            return this;
+       };
+    }(jQuery));
