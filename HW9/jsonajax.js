9fb2b57477c9406b057914e5992fa02bbf2c7f95
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-28 21:59:40 -0600

    Update jsonajax.js

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
             $("button").click(function () {
                 $("#pokeInfo").load("data/pokedex.json", function(responseText){
                     var pokemon = JSON.parse(responseText);
-                    $("#pokeInfo").html("Number:" + pokemon.num + "Name:" + pokemon.name + "Type:" 
-                    + pokemon.type[0] + pokemon.type[1] + "Height:" + pokemon.height + "Weight:" + pokemon.weight);
+                    $("#pokeInfo").html("Number:" + pokemon.num + "Name:" + pokemon.name + "Type:" + pokemon.type[0] + pokemon.type[1] + "Height:" + pokemon.height + "Weight:" + pokemon.weight);
                 });
             });
         });
