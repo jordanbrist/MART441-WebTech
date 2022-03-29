0508b9e38b52e332e7915a937be479aa8cb9892b
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-28 23:39:07 -0600

    Update jsonajax.js

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -1,13 +1,25 @@
-$("button").click(function () {
-    $.getJSON("pokedex.json", function (data) {
-        var pokedex;
-        $.each(data, function (i, pokemon) {
-            pokedex = $('<tr/>');
-            pokedex.append("<td>" + pokemon.name + "</td>");
-            pokedex.append("<td>" + pokemon.num + "</td>");
-            pokedex.append("<td>" + pokemon.height + "</td>");
-            pokedex.append("<td>" + pokemon.weight + "</td>");
-            $('table').append(pokedex);
+$(document).ready(function () {
+    $("button").click(function () {
+        $.getJSON("pokedex.json", function (data) {
+            var pokedex;
+            $.each(data, function (i, pokemon) {
+                pokedex = $('<tr/>');
+                pokedex.append("<td>" + pokemon.name + "</td>");
+                pokedex.append("<td>" + pokemon.num + "</td>");
+                pokedex.append("<td>" + pokemon.height + "</td>");
+                pokedex.append("<td>" + pokemon.weight + "</td>");
+                $('table').append(pokedex);
+            });
         });
     });
 });
+
+
+(function ($) {
+    $.fn.green = function () {
+        this.css("background-color", "green");
+        this.css("color", "white");
+        this.css("font-size", 40);
+        return this;
+    };
+}(jQuery));
