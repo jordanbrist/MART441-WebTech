cc2c67a872f49de931eafc5e3a17bf0b2528a1bd
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-29 00:10:39 -0600

    Update jsonajax.js

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
     $("button").click(function () {
         $.getJSON("pokedex.json", function (data) {
             $.each(data, function (i, pokemon) {
-                $('<tr/>').append("<td>" + pokemon.name + "</td>");
-                $('<tr/>').append("<td>" + pokemon.num + "</td>");
-                $('<tr/>').append("<td>" + pokemon.height + "</td>");
-                $('<tr/>').append("<td>" + pokemon.weight + "</td>");
+                $("<tr/>").append("<td>" + pokemon.name + "</td>");
+                $("<tr/>").append("<td>" + pokemon.num + "</td>");
+                $("<tr/>").append("<td>" + pokemon.height + "</td>");
+                $("<tr/>").append("<td>" + pokemon.weight + "</td>");
+                $('table').append("<tr/>");
             });
         });
     });
