d52f8430858cdd60bdcedbb9a0ed92b0b5a8ee96
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-28 23:09:11 -0600

    trying something

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -1,12 +1,20 @@
-
+var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+              //readyState == 4 means the request is finished and response is ready
+              //status == 200 "OK"
+              //When readyState is 4 and status is 200, the response is ready:
+                if (this.readyState == 4 && this.status == 200) {
 $(document).ready(function () {
     $("button").click(function () {
-        $("#pokeInfo").load("pokedex.json", function(responseText){
+        $("#pokeInfo").load("pokedex.json", function (responseText) {
             var pokemon = JSON.parse(responseText);
-            $("#pokeInfo").html("Name: " + pokemon.name 
-        + "<br>Number:" + pokemon.num + "<br>Height:" + pokemon.height + "<br>Weight:" 
-        + pokemon.weight + "<br>Type:" +
-        pokemon.type[0] + "<br>" + pokemon.type[1]);
+            $("#pokeInfo").html("Name: " + pokemon.name
+                + "<br>Number:" + pokemon.num + "<br>Height:" + pokemon.height + "<br>Weight:"
+                + pokemon.weight);
         });
     });
 });
+
+xhttp.open("GET", "pokedex.json", true); 
+            xhttp.send();
+        }}
