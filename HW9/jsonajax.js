43d18d946dd6a079b842711e6c49df15a7742987
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-03-28 22:16:34 -0600

    Update jsonajax.js

--- HW9/jsonajax.js
+++ HW9/jsonajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("button").click(function () {
-        $.getJSON("data/pokedex.json", function (data) {
+        $.getJSON("../data/pokedex.json", function (data) {
             var row;
             $.each(data, function (i, index) {
                 row = $('<tr/>');
