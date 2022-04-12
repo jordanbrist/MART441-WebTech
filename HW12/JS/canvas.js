0d88caac6a03838298653de9a362c7abdd7c30ff
jordanbrist <77375746+jordanbrist@users.noreply.github.com>
2022-04-12 03:39:41 -0600

    IT MOVES!!!

--- HW12/JS/canvas.js
+++ HW12/JS/canvas.js
@@ -24,13 +24,15 @@ function setup() {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
 
+    square1 = new Square(x, y, 20, 20, "blue");
+
     collectibles1 = new Square(200, 200, 25, 25, "yellow");
     $.getJSON("DATA/collectibles.json", function (data) {
         for (var i = 0; i < data.collectibles.length; i++) {
             collectiblesArray.push(new Square(data.collectibles[i].x, data.collectibles[i].y, data.collectibles[i].h, data.collectibles[i].w, data.collectibles[i].color));
         }})
 
-    square1 = new Square(x, y, 20, 20, "blue");
+    
     square2 = new Square(400, 400, 100, 100, "red");
     $.getJSON("DATA/squares.json", function (data) {
         for (var i = 0; i < data.squares.length; i++) {
@@ -40,83 +42,12 @@ function setup() {
     });
 }
 
-    function getKey(event) {
-
-        var char = event.which || event.keyCode;
-        var actualLetter = String.fromCharCode(char);
-        if (actualLetter == "w" && y + square1.height > 0) {
-            moveUp();
-        } else if (actualLetter == "s" && y + square1.height < canvas.height) {
-            moveDown();
-        } else if (actualLetter == "d" && x + square1.width < canvas.width) {
-            moveRight();
-        } else if (actualLetter == "a" && x + square1.width > 0) {
-            moveLeft();
-        }
-
-        var test = hasCollided(square1,square2);
-        var test2 = false;
-        for(var i = 0; i < squareArray.length; i++)
-        {
-    
-            test2 = hasCollided(square1,squareArray[i]);
-            if(test2 == true)
-            {
-                break;
-            }
-            
-            //console.log(test2);
-        }
-        if(test || test2)
-        {
-            lives--;
-            if(direction == "left")
-            {
-                moveRight();
-            }
-            else if(direction == "right")
-            {
-                moveLeft();
-            }
-            else if(direction == "up")
-            {
-                moveDown();
-            }
-            else if(direction == "down")
-            {
-                moveUp();
-            }
-        
-        }
-        drawSquare();
-
-    }
-
-    function moveUp()
-    {
-        square1.y-=10;
-    }
-    function moveDown()
-    {
-        square1.y+=10;
-    }
-    function moveRight()
-    {
-        square1.x+=10;
-    }
-    function moveLeft()
-    {
-        square1.x-=10;
-    }
-
-    drawSquare();
-
-
-
 function drawSquare() {
     ctx.clearRect(0, 0, 800, 600);
+    
     ctx.fillStyle = square1.color;
-    ctx.fillRect(x, y, 20, 20);
+    ctx.fillRect(square1.theX, square1.theY, square1.theWidth, square1.theHeight);
+
     ctx.fillStyle = square2.color;
     ctx.fillRect(square2.x, square2.y, square2.w, square2.h);
     for (var i = 0; i < squareArray.length; i++) {
@@ -136,6 +67,108 @@ function drawSquare() {
     ctx.fillText("Score: " + score, 10, 20);
 
 }
+
+    function getKey(event) {
+
+        var char = event.which || event.keyCode;
+        var actualLetter = String.fromCharCode(char);
+        if (actualLetter == "w") 
+        {
+            moveUp();
+        } 
+        else if (actualLetter == "s") 
+        {
+            moveDown();
+        } 
+        else if (actualLetter == "a") 
+        {
+            moveLeft();
+        } 
+        else if (actualLetter == "d") 
+        {
+            moveRight();
+        }
+
+        var test = hasCollided(square1,square2);
+    var test2 = false;
+    for(var i = 0; i < squareArray.length; i++)
+    {
+
+        test2 = hasCollided(square1,squareArray[i]);
+        if(test2 == true)
+        {
+            break;
+        }
+        
+        //console.log(test2);
+    }
+    if(test || test2)
+    {
+        lives--;
+        if(direction == "left")
+        {
+            moveRight();
+        }
+        else if(direction == "right")
+        {
+            moveLeft();
+        }
+        else if(direction == "up")
+        {
+            moveDown();
+        }
+        else if(direction == "down")
+        {
+            moveUp();
+        }
+    
+    }
+
+    var test3 = hasCollided(square1,collectibles1);
+    var test4 = false;
+    for(var i = 0; i < collectiblesArray.length; i++)
+    {
+
+        test4 = hasCollided(square1,collectiblesArray[i]);
+        if(test4 == true)
+        {
+            break;
+        }
+        
+        //console.log(test2);
+    }
+    if(test3 || test4)
+    {
+        score++;
+    }
+
+
+
+
+        drawSquare();
+
+    }
+
+    function moveUp() {
+        y-=10;
+        square1.setY(y);
+    }
+    
+    function moveDown() {
+        y+=10;
+        square1.setY(y);
+    }
+    
+    function moveLeft() {
+        x-=10;
+        square1.setX(x);
+    }
+    
+    function moveRight() {
+        x+=10;
+        square1.setX(x);
+    }
+    
 
 
 function hasCollided(object1, object2) {
